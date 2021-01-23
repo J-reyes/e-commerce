b8477cf1256040a3921120da8454c521f186b5d4
Jose Reyes <vazquez.jr09@gmail.com>
2021-01-23 11:59:27 -0800

    add signin page and form-input component + added styling

---
+++ src/components/sign-in/sign-in.component.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+import FormInput from "../form-input/form-input.component";
+
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    // prevent a refresh
+    event.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  // pull values
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            label="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            required
+          />
+          <label>Email</label>
+          <FormInput
+            name="password"
+            type="password"
+            handleChange={this.handleChange}
+            value={this.state.password}
+            label="password"
+            required
+          />
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
