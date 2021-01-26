84ae5175580a7880137b8a9377d3c2ad1b8f2e9d
Jose Reyes <vazquez.jr09@gmail.com>
2021-01-25 20:52:24 -0800

    implemented signup. Added signup to signin&signup component + added styles

--- src/components/sign-up/sign-up.component.js
+++ src/components/sign-up/sign-up.component.js
@@ -66,7 +66,7 @@ class SignUp extends React.Component {
       <div className="sign-up">
         <h2 className="title">I do not have a account</h2>
         <span>Sign up with your email and password</span>
-        <form className="sign-up-form" onSubmit={thhis.handleSubmit}>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
           <FormInput
             type="text"
             name="displayName"
