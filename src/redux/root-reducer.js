772824da6028513a7262cbe0c533f5d14d79b1e0
Jose Reyes <vazquez.jr09@gmail.com>
2021-02-15 23:44:42 -0800

    created + implemented directory reducer and selectors

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -8,19 +8,21 @@ import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 // persist config we want redux-persist to use
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
   // contains the name of the reducers we want to store(persist)
-  whitelist: ['cart']
-}
+  whitelist: ["cart"],
+};
 
-// setting root reduxer 
+// setting root reduxer
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
