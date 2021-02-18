4ef72c9b2a864c3eba76e0f04422f62444ecf759
Jose Reyes <vazquez.jr09@gmail.com>
2021-02-17 22:42:53 -0800

    completed mapping selectCollection selector + logic to dynamically load the correct pathh

--- src/pages/collection/collection.component.js
+++ src/pages/collection/collection.component.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+
 import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import {selectCollection} from '../../redux/shop/shop.selectors';
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionId);
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
   return (
     <div className="collection-page">
       <h2>Collection PAGE</h2>
@@ -12,4 +16,12 @@ const CollectionPage = ({ match }) => {
   );
 };
 
-export default CollectionPage;
+// ownProps are the props from the component we wrap in connect()
+const mapStateToProps = (state, ownProps) => ({
+  // adding state here is necessary because this selector needs a part
+  //  of the state depending on the URL parameter
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+  // ^^ we are passing the function returned by the function - state as a prop
+})
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,32 @@
 import { createSelector } from "reselect";
 
+// maps the string value to the respective id
+// were string value = the actual property
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mems: 5,
+};
+
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+// selector for mmatching category
+// pass in the URL params as collectionUrlParam which is a string
+export const selectCollection = (collectionUrlParam) =>
+// queried function
+  createSelector(
+    [selectCollections],
+    // will runs till the .find() returns true and will give us that element
+    (collections) =>
+      collections.find(
+        (collections) =>
+          collections.id === COLLECTION_ID_MAP[collectionUrlParam]
+      )
+  ); 
