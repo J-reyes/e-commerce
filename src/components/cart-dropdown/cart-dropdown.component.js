1594b48c0d6c8ac0ab1f0e9d098d6dfa6dd2a11a
Jose Reyes <vazquez.jr09@gmail.com>
2021-03-02 23:29:56 -0800

    added cart-dropdown styled component

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -4,35 +4,40 @@ import { createStructuredSelector } from "reselect";
 // Higher order component
 import { withRouter } from "react-router-dom";
 
-import CustomButton from "../custom-button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-import "./cart-dropdown.styles.scss";
+
+import {
+  CartDropDownContainer,
+  CartDropdownButton,
+  CartItemsContainer,
+  EmptyMessage
+} from "./cart-dropdown.styles";
 
 const CartDropDown = ({ cartItems, history, dispatch }) => {
   return (
-    <div className="cart-dropdown">
-      <div className="cart-items">
+    <CartDropDownContainer>
+      <CartItemsContainer>
         {cartItems.length ? (
           cartItems.map((cartItem) => (
             <CartItem key={cartItems.id} item={cartItem} />
           ))
         ) : (
-          <span className="empty-message">Your cart is empty</span>
+          <EmptyMessage>Your cart is empty</EmptyMessage>
         )}
-      </div>
+      </CartItemsContainer>
       {/* history.push comes from our withRouter HOC */}
-      <CustomButton
+      <CartDropdownButton
         onClick={() => {
           history.push("/checkout");
           dispatch(toggleCartHidden()); // short hand dispatch
         }}
       >
         GO TO CHECKOUT
-      </CustomButton>
-    </div>
+      </CartDropdownButton>
+    </CartDropDownContainer>
   );
 };
 
---
+++ src/components/cart-dropdown/cart-dropdown.styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import CustomButton from "../custom-button/button.component";
+
+export const CartDropDownContainer = styled.div`
+  position: absolute;
+  width: 240px;
+  height: 340px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid black;
+  background-color: white;
+  top: 90px;
+  right: 40px;
+  z-index: 5;
+`;
+
+export const CartDropdownButton = styled(CustomButton)`
+  margin-top: auto;
+`;
+
+export const CartItemsContainer = styled.div`
+  height: 240px;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    -webkit-appearance: none;
+    width: 7px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: rgba(0, 0, 0, 0.5);
+    box-shadow: 0 0 1px rgba(255, 255, 255, 0.5);
+  }
+`;
+
+export const EmptyMessage = styled.span`
+  font-size: 18px;
+  // up-down lt-rt
+  margin: 50px auto;
+`;
