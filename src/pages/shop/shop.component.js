3314e4b9af1aaf4510c7a55749e834fcdce6046d
Jose Reyes <vazquez.jr09@gmail.com>
2021-03-12 21:46:01 -0800

    added a temp way to display our spinner

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -4,40 +4,58 @@ import { connect } from "react-redux";
 
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
+import { updateCollections } from "../../redux/shop/shop.actions";
 
 import {
   firestore,
   convertCollectinosSnapshotToMap,
 } from "../../firebase/firebase.utils";
 
-import { updateCollections } from "../../redux/shop/shop.actions";
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+  // temp for now w/ out writing constructor
+  state = {
+    loading: true,
+  };
+
   unsubscribeFromSnapShot = null;
 
   // fetch snapshot of the collection array using componentDidMount
   componentDidMount() {
-    const {updateCollections} = this.props;
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
     // get data when ever collectionRef updates
     // or code runs for thhe first time -> ref will send us the snapshot
     collectionRef.onSnapshot(async (snapshot) => {
       const collectionsMap = convertCollectinosSnapshotToMap(snapshot);
-      updateCollections(collectionsMap)
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
   }
 
   render() {
     const { match } = this.props;
-
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         {/* dynamically get the correct category from reducer */}
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
