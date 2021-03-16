956646ed28191248fd6ac91c0d2134a5d72f213f
Jose Reyes <vazquez.jr09@gmail.com>
2021-03-15 22:36:51 -0700

    setup thunk in our redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,11 +5,12 @@ import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 // debuggin
 import logger from "redux-logger";
+import thunk from 'redux-thunk'
 
 import rootReducer from "./root-reducer";
 
 // middleware setup
-const middlewares = [];
+const middlewares = [thunk];
 
 // make sure to only output logs when in dev environment
 if (process.env.NODE_ENV === 'development') {
