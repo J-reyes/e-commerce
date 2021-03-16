f24a04e5b8a03ddbb395cd55778b61ef9ef41567
Jose Reyes <vazquez.jr09@gmail.com>
2021-03-15 23:04:54 -0700

    added our fetch actions - updated our reducer with our fetch actions - added fetch types.

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,31 @@
-import ShopActionTypes from './shop.types';
+import ShopActionTypes from "./shop.types";
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
-  payload: collectionsMap
-})
+import {
+  firestore,
+  convertCollectinosSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  // returning function that gets a dispatch
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    // dispatch start call
+    dispatch(fetchCollectionsStart());
+
+    // gets data associated to collectionRef
+    // using .then() bbecause now it is a promise
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectinosSnapshotToMap(snapshot);
+      dispatch(fetchCollectionsSuccess(collectionsMap));
+    });
+  };
+};
--- src/redux/shop/shop.types.js
+++ src/redux/shop/shop.types.js
@@ -1,5 +1,10 @@
 const ShopActionTypes = {
-  UPDATE_COLLECTIONS: 'UPDATE_COLLECTIONS'
+  // Start of our collection fetch
+  FETCH_COLLECTIONS_START: 'FETCH_COLLECTIONS_START',
+  // success comes back with a successfull api request
+  FETCH_COLLECTIONS_SUCCESS: 'FETCH_COLLECTIONS_SUCCESS',
+  // if thhere is an error with our api call
+  FETCH_COLLECTIONS_FAILURE: 'FETCH_COLELCTIONS_FAILURE'
 }
 
 export default ShopActionTypes;
