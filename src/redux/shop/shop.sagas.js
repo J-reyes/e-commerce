09adc0f45d5df0a6549d0c3c79b223a0e7bb38c6
Jose Reyes <vazquez.jr09@gmail.com>
2021-04-27 19:29:54 -0700

    Start to implement root saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,16 +1,38 @@
 // import affects from sagas - to do different things w/ the store
 // creating actions or listening for actions
-import { takeEvery } from "redux-saga/effects";
+import { takeLatest, call, put } from "redux-saga/effects";
+import {
+  firestore,
+  convertCollectinosSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
 
 import ShopActionTypes from "./shop.types";
 
 export function* fetchCollectionsAsync() {
-  yield console.log("I am fired");
+  try {
+    const collectionRef = firestore.collection("collections");
+    // comes back in a promise form
+    const snapshot = yield collectionRef.get();
+    // first arg - is a method, second is the params we are passing
+    const collectionsMap = yield call(
+      convertCollectinosSnapshotToMap,
+      snapshot
+    );
+    // dispatched out an object
+    yield put(fetchCollectionsSuccess(collectionsMap))
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message))
+  }
 }
 
 export function* fetchCollectionsStart() {
   // will pause whenever a specific actioon comes in
-  yield takeEvery(
+  yield takeLatest(
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
