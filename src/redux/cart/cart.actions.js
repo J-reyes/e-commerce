84150ccca9c2a42d1f420e32ffe02892788b2683
Jose Reyes <vazquez.jr09@gmail.com>
2021-05-04 21:41:15 -0700

    Created clear cart saga

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,23 +1,27 @@
-import {CartActionTypes} from "./cart.types";
+import { CartActionTypes } from "./cart.types";
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
   // payload - is an optional property
 });
 
-export const addItem = item => ({
+export const addItem = (item) => ({
   // type is the action(function) we will perform
   type: CartActionTypes.ADD_ITEM,
   // payload is the item we want to add to our cart array
-  payload: item
-})
+  payload: item,
+});
 
-export const clearItemFromCart = item => ({
+export const clearItemFromCart = (item) => ({
   type: CartActionTypes.CLEAR_ITEM_FROM_CART,
-  payload: item
-})
+  payload: item,
+});
 
-export const removeItem = item => ({
+export const removeItem = (item) => ({
   type: CartActionTypes.REMOVE_ITEM,
-  payload: item
-})
+  payload: item,
+});
+
+export const clearCart = () => ({
+  type: CartActionTypes.CLEAR_CART,
+});
