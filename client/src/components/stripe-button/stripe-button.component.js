f0a4e31f562ef70dd70bd283f3e041a59e37af45
Jose Reyes <vazquez.jr09@gmail.com>
2021-05-31 20:51:58 -0700

    Added backend + setup payment route

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   // checkout value must bbe in cents
@@ -9,8 +10,23 @@ const StripeCheckoutButton = ({ price }) => {
 
   // taken token => to pass to backend
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please be sure you use the provided credit card"
+        );
+      });
   };
 
   return (
