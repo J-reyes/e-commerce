18d664852e3b2541a25fcf1efd9ee381957c3207
Jose Reyes <vazquez.jr09@gmail.com>
2021-06-03 21:34:00 -0700

    Refactored head.component + App.js to use Context API to set the currentUser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./App.css";
 import { Switch, Route, Redirect } from "react-router-dom";
-// redux
-import { connect } from "react-redux";
-// auto passes state by pointing to the correct selector
-import { createStructuredSelector } from "reselect";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
@@ -12,22 +8,23 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from "./components/header/header.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
 
-import {
-  auth,
-  createUserProfileDocument,
-} from "./firebase/firebase.utils";
-// action from redux
-import { setCurrentUser } from "./redux/user/user.actions";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
-// selector
-import { selectCurrentUser } from "./redux/user/user.selector";
+import CurrentUserContext from "./contexts/current-user/current-user.context";
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    // local state
+    this.state = {
+      currentUser: null,
+    };
+  }
+
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
-
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       // if user exists
       if (userAuth) {
@@ -38,14 +35,17 @@ class App extends React.Component {
         userRef.onSnapshot((snapShot) => {
           // setting the user reducer value when
           // snapshot value updates
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data(),
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
           });
         });
       } else {
-        // we just need the object
-        setCurrentUser(userAuth);
+        // else use doesn't yet exist
+        // setState
+        this.setState({ currentUser: userAuth });
       }
     });
   }
@@ -58,7 +58,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <CurrentUserContext.Provider value={this.state.currentUser}>
+          <Header />
+        </CurrentUserContext.Provider>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -67,7 +69,7 @@ class App extends React.Component {
             exact
             path="/signin"
             render={() =>
-              this.props.currentUser ? (
+              this.state.currentUser ? (
                 <Redirect to="/" />
               ) : (
                 <SignInAndSignUpPage />
@@ -80,12 +82,4 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 // Higher order component (takes component as an argument)
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -11,6 +11,8 @@ import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 // selectors
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selector";
+// user context
+import CurrentUserContext from "../../contexts/current-user/current-user.context";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
@@ -22,7 +24,9 @@ import {
 } from "./header.styles";
 
 // currentUser prop from auth library
-const Header = ({ currentUser, hidden }) => {
+const Header = ({ hidden }) => {
+  const currentUser = useContext(CurrentUserContext);
+
   return (
     <HeaderContainer>
       <LogoContainer className="logo-container" to="/">
@@ -30,14 +34,10 @@ const Header = ({ currentUser, hidden }) => {
       </LogoContainer>
       {/* nav links */}
       <OptionsContainer>
-        <OptionLink to="/shop">
-          SHOP
-        </OptionLink>
-        <OptionLink to="/shop">
-          CONTACT
-        </OptionLink>
+        <OptionLink to="/shop">SHOP</OptionLink>
+        <OptionLink to="/shop">CONTACT</OptionLink>
         {currentUser ? (
-          <OptionLink as='div'  onClick={() => auth.signOut()}>
+          <OptionLink as="div" onClick={() => auth.signOut()}>
             SIGN OUT
           </OptionLink>
         ) : (
@@ -55,7 +55,6 @@ const Header = ({ currentUser, hidden }) => {
 // pass in entire state instead of just a portion
 const mapStateToProps = createStructuredSelector({
   // createStructuredSelector points to the correct selector and auto passes our top level state
-  currentUser: selectCurrentUser,
   hidden: selectCartHidden,
 });
 
